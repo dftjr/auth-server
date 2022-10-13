f1ffa60b6cd3be87fcb2678959788f2dfc201b12
DAVID TUSIA <DAVE.TUSIA@GMAIL.COM>
2022-10-13 12:45:27 +0000

    Code updated to run on localhost 3001

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 // Start up DB Server
 const { db } = require('./src/auth/models/index.js');
+const { startup } = require('./src/server');
+const PORT = process.env.PORT || 3001;
 db.sync()
-  .then(() => {
+  .then(() => 
 
     // Start the web server
-    require('./src/server.js').start(process.env.PORT);
-  });
+    startup(PORT)
+  );
